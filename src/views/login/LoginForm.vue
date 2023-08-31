ddaa10e6be2f69be0db38bb3e0e9e59ba3e05696
zhangzimin <1755488764@qq.com>
2023-08-31 16:28:01 +0800

    feat: login logic

--- src/views/login/LoginForm.vue
+++ src/views/login/LoginForm.vue
@@ -2,6 +2,8 @@
 import { login } from "@/api/user";
 import { message } from "ant-design-vue";
 import { useForm } from "@/libs/hooks/useForm";
+import GraphValidateCodeImage from '@/components/GraphValidateCodeImage'
+import { nextTick } from "vue";
 const { t } = useI18n();
 
 const formState = reactive({
@@ -11,20 +13,31 @@ const formState = reactive({
 const rules = computed(() => ({
   name: [{ required: true, message: t("8dRn48_9RTO6Q2804fgFp") }],
   password: [{ required: true, message: t("8dRn48_9RTO6Q2804fgFp") }],
+  code: [
+    { required: true, message: t("8dRn48_9RTO6Q2804fgFp") },
+    { len: 4, message: t("cWWvrwCrLJY6luPt-0_h8") },
+  ],
 }));
 const loading = ref(false);
 
-const emit = defineEmits(['reset', 'mfa'])
+const emit = defineEmits(["reset", "mfa"]);
 
-const { handleValidate, validateInfos } = useForm(formState, rules)
+const { handleValidate, validateInfos } = useForm(formState, rules);
 
 const router = useRouter();
+const validateCode = ref(false);
+const graphCode = ref();
 const handleSubmit = async () => {
-  const { values } = await handleValidate()
+  const { values } = await handleValidate(
+    validateCode.value ? ["name", "password", "code"] : ["name", "password"]
+  );
   if (values) {
-    loading.value = true
-    const { statusCode, content } = await login({ ...values, name: values.name.toUpperCase() })
-    loading.value = false
+    loading.value = true;
+    const { statusCode, content } = await login({
+      ...values,
+      name: values.name.toUpperCase(),
+    });
+    loading.value = false;
     if (statusCode === 200) {
       const {
         // need mfa
@@ -38,41 +51,31 @@ const handleSubmit = async () => {
       if (ifCheckGoogleSecretKey) {
         if (ifGoogleSecretKeyBound) {
           if (ifFirstLogin) {
-            emit('reset', name)
+            emit("reset", name);
           } else {
-            emit('mfa')
+            emit("mfa");
           }
         } else {
-          message.warning(t('8jnmiDecv_inomSIVKpDF'))
+          message.warning(t("8jnmiDecv_inomSIVKpDF"));
         }
       } else {
         if (ifFirstLogin) {
           // reset password
-				  emit('reset', name)
+          emit("reset", name);
         } else {
-          router.replace({ name: 'Dashboard' })
+          router.replace({ name: "Dashboard" });
         }
       }
     } else {
-      const { codeFlag } = content;
-      if (codeFlag) {
-        if (firstValidateCode) {
-          validateCode.value = true;
-          Object.assign(rules, {
-            code: [
-              { required: true, message: t("8dRn48_9RTO6Q2804fgFp") },
-              { len: 4, message: t("cWWvrwCrLJY6luPt-0_h8") },
-            ],
-          });
-          firstValidateCode = false;
-        } else {
+      if (content.codeFlag) {
+        validateCode.value = true;
+        nextTick(() => {
           graphCode.value.refreshCode();
-        }
+        });
       }
     }
   }
-}
-
+};
 </script>
 <template>
   <a-form class="mx-auto max-w-[368px]">
@@ -100,7 +103,7 @@ const handleSubmit = async () => {
     </a-form-item>
 
     <template v-if="validateCode">
-      <a-form-item>
+      <a-form-item >
         <a-form-item
           v-bind="validateInfos.code"
           :style="{ display: 'inline-block', width: '248px' }"
@@ -121,7 +124,7 @@ const handleSubmit = async () => {
             verticalAlign: 'middle',
           }"
         >
-          <GraphValidateCodeImage open ref="graphCode" />
+          <GraphValidateCodeImage ref="graphCode" />
         </a-form-item>
       </a-form-item>
     </template>
@@ -133,7 +136,7 @@ const handleSubmit = async () => {
       block
       :loading="loading"
     >
-      {{ $t('0qjtHGUfIBLV4LzHX8WWD') }}
+      {{ $t("0qjtHGUfIBLV4LzHX8WWD") }}
     </a-button>
   </a-form>
 </template>
