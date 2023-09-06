59f4bfd9c3959ce1094059afc846545f73841e52
张殃离 <96040653+ZhangYangLizzm@users.noreply.github.com>
2023-09-06 21:38:08 +0800

    update

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,10 @@
 <template>
   <div class="w-3/4 h-3/4 bg-white rounded-xl shadow flex">
-    <div class="w-[200px] h-full left-siderbar">
-      <AButton type="primary" @click="visible = !visible" class="bg-purple-300">
-        抽屉</AButton
-      >
-    </div>
-    <div class="layout-content w-full flex" id="design-layout">
-      <div class="flex-grow-[2] bg-red-100 relative"></div>
+    <div class="w-[200px] h-full left-siderbar p-4">LeftSidegbar</div>
+    <div class="layout-content w-full flex rounded-r-xl" id="design-layout">
+      <div class="flex-grow relative grow-transition bg-red-200 p-4">
+        <AButton type="primary" @click="visible = !visible"> 抽屉</AButton>
+      </div>
     </div>
   </div>
 
@@ -25,5 +23,13 @@ const CustomDrawer = defineAsyncComponent(
 <style scoped>
 #design-layout {
   position: relative;
+}
+
+#design-layout:has(.custom-drawer) > div {
+  flex-grow: 2;
+}
+
+.left-siderbar {
+  border-right: 1px solid rgba(182, 178, 178, 0.392);
 }
 </style>
--- src/components/CustomDrawer.vue
+++ src/components/CustomDrawer.vue
@@ -2,15 +2,15 @@
   <Teleport :to="getContainer">
     <transition name="slide-grow">
       <div
-        class="custom-drawer"
+        class="custom-drawer rounded-r-xl"
         v-if="visible"
         :class="[visible ? 'flex-grow' : 'flex-grow-0']"
       >
         <div class="relative flex-grow">
-          <div class="drawer-header">
+          <div class="drawer-header p-4">
             <slot name="header"> Header</slot>
           </div>
-          <div class="drawer-content">
+          <div class="drawer-content p-4">
             <slot></slot>
           </div>
         </div>
@@ -27,4 +27,12 @@ defineProps({
   },
 });
 </script>
-<style scoped lang="less"></style>
+<style scoped>
+.custom-drawer {
+  border-left: 1px solid rgba(182, 178, 178, 0.392);
+}
+
+.drawer-header {
+  border-bottom: 1px solid rgba(182, 178, 178, 0.392);
+}
+</style>
